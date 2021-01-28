62dd37b5829d02bcfd43476804c056b632ced87d
ELISHA1994 <elishabello2014@gmail.com>
2021-01-28 22:28:13 +0100

    Starter app

---
+++ server/server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+import handlers from './routes/handlers'
+
+const port = process.env.PORT || 1337
+
+// App instance variables
+const app = express();
+
+app.use(logger('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/', handlers.whateverroutehandler)
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
+
+export default app;
