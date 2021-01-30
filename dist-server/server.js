dd75c7646035934cc5562c4144683833033be1e2
ELISHA1994 <elishabello2014@gmail.com>
2021-01-30 16:38:03 +0100

    Validation route Complete

--- dist-server/server.js
+++ dist-server/server.js
@@ -17,6 +17,10 @@ var _cors = _interopRequireDefault(require("cors"));
 
 var _bodyParser = _interopRequireDefault(require("body-parser"));
 
+var _middlewares = _interopRequireDefault(require("./middlewares/middlewares"));
+
+var _routes = _interopRequireDefault(require("./routes/routes"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 // import handlers from './routes/handlers'
@@ -25,14 +29,16 @@ var port = process.env.PORT || 1337; // App instance variables
 var app = (0, _express["default"])();
 app.use((0, _morgan["default"])('dev'));
 app.use((0, _cors["default"])());
-app.use(_express["default"].json());
 app.use(_bodyParser["default"].json());
 app.use(_express["default"].urlencoded({
   extended: false
 }));
-app.use((0, _cookieParser["default"])());
-app.use(_express["default"]["static"](_path["default"].join(__dirname, '../public')));
-app.get('/');
+app.use((0, _cookieParser["default"])()); // app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/', _routes["default"].details);
+app.post('/validate-rule', _routes["default"].validateRule);
+app.use(_middlewares["default"].handleError);
+app.use(_middlewares["default"].notFound);
 app.listen(port, function () {
   console.log("Server started on port ".concat(port));
 });
--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ import logger from 'morgan';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 
+import middleware from './middlewares/middlewares';
+import routes from './routes/routes'
 // import handlers from './routes/handlers'
 
 const port = process.env.PORT || 1337
@@ -14,13 +16,16 @@ const app = express();
 
 app.use(logger('dev'));
 app.use(cors());
-app.use(express.json());
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
+// app.use(express.static(path.join(__dirname, '../public')));
 
-app.get('/')
+app.get('/', routes.details)
+app.post('/validate-rule', routes.validateRule)
+
+app.use(middleware.handleError);
+app.use(middleware.notFound);
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
